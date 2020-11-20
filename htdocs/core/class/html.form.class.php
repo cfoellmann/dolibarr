558cd01a5143749b8d20a7987f681cbe74ce2366
Christian Foellmann <foellmann@foe-services.de>
2020-11-20 15:59:13 +0100

    PR allow $force_entity on form select if multicompany

--- htdocs/core/class/html.form.class.php
+++ htdocs/core/class/html.form.class.php
@@ -1689,7 +1689,8 @@ class Form
 			{
 				$sql .= " LEFT JOIN ".MAIN_DB_PREFIX."usergroup_user as ug";
 				$sql .= " ON ug.fk_user = u.rowid";
-				$sql .= " WHERE ug.entity = ".$conf->entity;
+				if ($force_entity) $sql .= " WHERE ug.entity IN (0,".$force_entity.")";
+				else $sql .= " WHERE ug.entity = ".$conf->entity;
 			} else {
 				$sql .= " WHERE u.entity IN (0,".$conf->entity.")";
 			}
