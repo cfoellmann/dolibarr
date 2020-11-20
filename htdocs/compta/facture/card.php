bdf86a1523bc7b46662d9d32f1f446f0a5be0dd8
Christian Foellmann <foellmann@foe-services.de>
2020-11-20 15:59:40 +0100

    TEMP set $hidedesc = 1 to hide DESC for invoices

--- htdocs/compta/facture/card.php
+++ htdocs/compta/facture/card.php
@@ -93,6 +93,8 @@ $hidedetails = (GETPOST('hidedetails', 'int') ? GETPOST('hidedetails', 'int') : 
 $hidedesc = (GETPOST('hidedesc', 'int') ? GETPOST('hidedesc', 'int') : (!empty($conf->global->MAIN_GENERATE_DOCUMENTS_HIDE_DESC) ? 1 : 0));
 $hideref = (GETPOST('hideref', 'int') ? GETPOST('hideref', 'int') : (!empty($conf->global->MAIN_GENERATE_DOCUMENTS_HIDE_REF) ? 1 : 0));
 
+$hidedesc = 1;
+
 // Nombre de ligne pour choix de produit/service predefinis
 $NBLINES = 4;
 
