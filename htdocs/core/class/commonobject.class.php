fdda2c60d7cf1a88e8fd8bd9915a37ee475b35ea
Christian Foellmann <christian@foellmann.de>
2021-08-27 14:10:53 +0200

    fix BOM tpl loading

--- htdocs/core/class/commonobject.class.php
+++ htdocs/core/class/commonobject.class.php
@@ -4667,7 +4667,13 @@ abstract class CommonObject
 			// Output template part (modules that overwrite templates must declare this into descriptor)
 			// Use global variables + $dateSelector + $seller and $buyer
 			// Note: This is deprecated. If you need to overwrite the tpl file, use instead the hook.
-			$dirtpls = array_merge($conf->modules_parts['tpl'], array($defaulttpldir));
+//			$dirtpls = array_merge($conf->modules_parts['tpl'], array($defaulttpldir));
+			if ($defaulttpldir == '/bom/tpl') {
+				$dirtpls = array_merge(array(), array($defaulttpldir));
+			} else {
+				$dirtpls = array_merge($conf->modules_parts['tpl'], array($defaulttpldir));
+			}
+
 			foreach ($dirtpls as $module => $reldir) {
 				if (!empty($module)) {
 					$tpl = dol_buildpath($reldir.'/objectline_title.tpl.php');
@@ -4790,7 +4796,13 @@ abstract class CommonObject
 			// Output template part (modules that overwrite templates must declare this into descriptor)
 			// Use global variables + $dateSelector + $seller and $buyer
 			// Note: This is deprecated. If you need to overwrite the tpl file, use instead the hook printObjectLine and printObjectSubLine.
-			$dirtpls = array_merge($conf->modules_parts['tpl'], array($defaulttpldir));
+//			$dirtpls = array_merge($conf->modules_parts['tpl'], array($defaulttpldir));
+			if ($defaulttpldir == '/bom/tpl') {
+				$dirtpls = array_merge(array(), array($defaulttpldir));
+			} else {
+				$dirtpls = array_merge($conf->modules_parts['tpl'], array($defaulttpldir));
+			}
+
 			foreach ($dirtpls as $module => $reldir) {
 				if (!empty($module)) {
 					$tpl = dol_buildpath($reldir.'/objectline_view.tpl.php');
